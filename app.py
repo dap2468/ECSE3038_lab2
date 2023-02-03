31625d6066d87c3da5eb430b0707869143fc3244
dap2468 <dap2468@gmail.com>
2023-02-03 15:30:25 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    "https://ecse-week3-demo.netlify.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+fake_database = []
+
+@app.get("/todos")
+async def get_all_todos():
+    return fake_database
+
+@app.post("/todos")
+async def create_new_todo(request: Request):
+    todo= await request.json()
+    fake_database.append(todo)
+    return todo
+
+@app.patch("/todos/{id}")
+async def update_todo_by_id(id:int, request: Request):
+    todo_update= await request.json()
+    for todo in fake_database:
+        if id== todo['id']:
+            todo.update(todo_update)
+            return todo
+
+
+
+
